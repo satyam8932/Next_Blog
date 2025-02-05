44f81b3fe7eb304a681a8211cfa0dfb7c1ad9efc
Satyam Singh <satyamsingh5august@gmail.com>
2025-02-05 14:39:55 +0000

    linting disabled

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com", "avatar.vercel.sh", "res.cloudinary.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false
     return config
